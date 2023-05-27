3750a10857b220c76989c1095ae7d8af745806c9
A-Hannora <ahannora440@gmail.com>
2023-05-27 18:34:28 +0300

    creating the app and configure the main app files layout and create splash screen

--- lib/features/presentation/views/splash_view.dart
+++ lib/features/presentation/views/splash_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class SplashView extends StatelessWidget {
   const SplashView({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Container();
