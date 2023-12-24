641f9a41dc030a7c7571b5a1e4acf4a4b4c8cdb3
A-Hannora <ahannora440@gmail.com>
2023-12-25 00:37:20 +0200

    - add navigate to homepage - splash_screen_body_widget.dart code refactor

--- lib/features/Splash/presentation/views/widgets/splash_screen_body_widget.dart
+++ lib/features/Splash/presentation/views/widgets/splash_screen_body_widget.dart
@@ -1,9 +1,10 @@
-
-import 'package:bookly/core/utils/helper/assets_helper.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
+import '../../../../../contants.dart';
+import '../../../../../core/utils/helper/assets_helper.dart';
+import '../../../../Home/presentation/views/home_screen_page.dart';
 import 'fade_animated_text.dart';
 
 class SplashScreenBodyWidget extends StatefulWidget {
@@ -21,17 +22,37 @@ class _SplashScreenBodyWidgetState extends State<SplashScreenBodyWidget>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-        vsync: this, duration: const Duration(seconds: 2));
-    fadeAnimation = Tween<double>(begin: 0.0 , end: 1.0)
-        .animate(animationController);
+    initStartFadingAnimation(); //start the animation
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(
+          const HomePage(),
+          duration: kTransitionDuration,
+          transition: Transition.fade,
+        );
+      },
+    );
+  }
+
+  void initStartFadingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    fadeAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(animationController);
     animationController.forward(); //start the animation
   }
+
   @override
   void dispose() {
     super.dispose();
     animationController.dispose(); //release the resources
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
